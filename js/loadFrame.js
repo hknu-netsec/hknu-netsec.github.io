8a44d1306f3e820ab59f9ab6117e7ef800b4806d
Junhyeok Yun <junhyeok.dev@gmail.com>
2018-05-28 11:33:43 +0900

    fix: Replace initial load method with window.onload and change href adresses to HTTPS

--- js/loadFrame.js
+++ js/loadFrame.js
@@ -1,4 +1,4 @@
-$(function () {
+$(window).on("load", function () {
     $("#headerFrame").load("https://hknu-netsec.github.io/frame/header.html");
     $("#footerFrame").load("https://hknu-netsec.github.io/frame/footer.html");
 });
