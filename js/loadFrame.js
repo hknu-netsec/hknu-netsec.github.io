4102cbb97889d50b2d36939e9ae9f72178dcecd0
Junhyeok Yun <junhyeok.dev@gmail.com>
2018-05-28 11:20:03 +0900

    feat: Add frame load mechanism

---
+++ js/loadFrame.js
@@ -0,0 +1,4 @@
+$(function () {
+    $("#headerFrame").load("frame/header.html");
+    $("#footerFrame").load("frame/footer.html");
+});
--- js/navLink.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#logo").on("click", function () {
-        location.replace("index.html");
-    });
-
-    $("#navMembers").on("click", function () {
-        location.replace("members.html");
-    })
-});
