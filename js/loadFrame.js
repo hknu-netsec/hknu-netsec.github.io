a2fc933e455b348c930bb0432b55a58d84ea585c
Junhyeok Yun <junhyeok.dev@gmail.com>
2018-05-30 13:57:20 +0900

    style: Fix typo

--- js/loadFrame.js
+++ js/loadFrame.js
@@ -2,6 +2,9 @@ $(window).on("load", function () {
     $("#mainFrame").load("https://hknu-netsec.github.io/docs/members.html");
 
     $("#navProfessor").on("click", function () {
-        $("#mainFrame").load("https://hknu-netsec.gihut.io/docs/professor.html")
+        $("#mainFrame").load("https://hknu-netsec.github.io/docs/professor.html");
+    })
+    $("#navMembers").on("click", function () {
+        $("#mainFrame").load("https://hknu-netsec.github.io/docs/members.html");
     })
 });
